8f7c6786934ab1a92b4095a475ff51a5bab6baf5
Nikyta <nikytaxat@gmail.com>
2017-12-04 21:46:34 +0200

    no message

---
+++ src/main/java/com/nikyta/Controller/DocsController.java
@@ -0,0 +1,15 @@
+package com.nikyta.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/doc")
+public class DocsController {
+
+
+
+}
